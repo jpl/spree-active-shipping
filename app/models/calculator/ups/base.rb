5ea026ba7fab302a7671d47316e501fc01a842c3
jplecaille <jplecaille@gmail.com>
2010-06-14 10:33:30 +0200

    Adding test option to ups calculcator

--- app/models/calculator/ups/base.rb
+++ app/models/calculator/ups/base.rb
@@ -1,7 +1,8 @@
 class Calculator::Ups::Base < Calculator::ActiveShipping
   def carrier
     ActiveMerchant::Shipping::UPS.new(:login => Spree::ActiveShipping::Config[:ups_login], 
-                                      :password => Spree::ActiveShipping::Config[:ups_password], 
-                                      :key => Spree::ActiveShipping::Config[:ups_key])      
+                                        :password => Spree::ActiveShipping::Config[:ups_password], 
+                                        :key => Spree::ActiveShipping::Config[:ups_key],
+                                        :test => Spree::ActiveShipping::Config[:test_mode])
   end
 end
